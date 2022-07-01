68d598dfa8bbbfe7cf9a17922324414903f997c5
philip r brenan <philiprbrenan@gmail.com>
2022-07-01 03:46:39 +0100

    Update select.py

--- select.py
+++ select.py
@@ -1,21 +1,17 @@
 import mysql.connector
 
 # Connect to server
-cnx = mysql.connector.connect(
-    host="127.0.0.1",
-    port=3306,
-    user="root",
-    password="root")
-
-# Get a cursor
-cur = cnx.cursor()
-
-# Execute a query
-cur.execute("SELECT CURDATE()")
-
-# Fetch one result
-row = cur.fetchone()
+cnx = mysql.connector.connect(host="127.0.0.1", port=3306, user="root", password="root")   # Connect
+cur = cnx.cursor()                                                                         # Create cursor
+cur.execute("SELECT CURDATE()")                                                            # Execute a query
+row = cur.fetchone()                                                                       # Fetch row 
 print("Current date is: {0}".format(row[0]))
 
-# Close connection
-cnx.close()
+cur.execute("CREATE TABLE people(name text, phone, text)")
+cur.execute("INSERT INTO  people values('aaa', 111)");
+cur.execute("INSERT INTO  people values('bbb', 222)");
+cur.execute("SELECT COUNT(*) FROM people");
+row = cur.fetchone()                                                                       # Fetch row 
+print(row)
+
+cnx.close()                                                                                # Close   
