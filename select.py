d9cc4bc3286ea655bb35a13d0672e3f7cc3efcd6
philip r brenan <philiprbrenan@gmail.com>
2022-07-01 03:59:03 +0100

    Update select.py

--- select.py
+++ select.py
@@ -6,6 +6,7 @@ cur = cnx.cursor()                                                              
 cur.execute("SELECT CURDATE()")                                                            # Execute a query
 row = cur.fetchone()                                                                       # Fetch row 
 print("Current date is: {0}".format(row[0]))
+
 cur.execute("CREATE DATABASE test")
 cur.execute("CREATE TABLE test.people(name text, phone text)")
 cur.execute("INSERT INTO  test.people values('aaa', 111)")
@@ -14,7 +15,20 @@ cur.execute("INSERT INTO  test.people values('ccc', 333)")
 cur.execute("INSERT INTO  test.people values('ddd', 444)")
 cur.execute("INSERT INTO  test.people values('eee', 555)")
 cur.execute("SELECT COUNT(*) FROM test.people")
-row = cur.fetchone()                                                                       # Fetch row 
+
+cur.execute("SELECT COUNT(*) FROM test.people")                                            # Count rows 
+row = cur.fetchone()                                                                        
 print("The number of rows is ", row[0])
+
+cur.execute("update test.people set phone = 666 where name = 'eee'");                      # Update
+ 
+cur.execute("SELECT * FROM test.people")
+row = cur.fetchone(); print(cur); print(row[0])
+row = cur.fetchone(); print(cur); print(row[0])
+row = cur.fetchone(); print(cur); print(row[0])
+row = cur.fetchone(); print(cur); print(row[0])
+row = cur.fetchone(); print(cur); print(row[0])
+row = cur.fetchone(); print(cur); print(row[0])
+row = cur.fetchone(); print(cur); print(row[0])
 
 cnx.close()                                                                                # Close   
