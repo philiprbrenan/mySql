198e2d3c708c35b9c0d1d3619d02e0b01da787d1
philip r brenan <philiprbrenan@gmail.com>
2022-07-01 03:47:05 +0100

    Update select.py

--- select.py
+++ select.py
@@ -8,9 +8,9 @@ row = cur.fetchone()                                                            
 print("Current date is: {0}".format(row[0]))
 
 cur.execute("CREATE TABLE people(name text, phone, text)")
-cur.execute("INSERT INTO  people values('aaa', 111)");
-cur.execute("INSERT INTO  people values('bbb', 222)");
-cur.execute("SELECT COUNT(*) FROM people");
+cur.execute("INSERT INTO  people values('aaa', 111)")
+cur.execute("INSERT INTO  people values('bbb', 222)")
+cur.execute("SELECT COUNT(*) FROM people")
 row = cur.fetchone()                                                                       # Fetch row 
 print(row)
 
